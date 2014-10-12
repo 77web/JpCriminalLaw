a27a059970a5ad3c6582134c0104d71843537db9
77web <info@77-web.com>
2014-10-12 21:01:36 +0900

    wip: 殺人罪を追加

---
+++ src/Entity/罪/殺人罪.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Jp\CriminalLaw\Entity\罪;
+
+
+class 殺人罪 implements 罪
+{
+    public function get構成要件()
+    {
+        // TODO
+    }
+
+    public function get法定刑()
+    {
+        // TODO
+    }
+
+    public function has過失犯処罰規定()
+    {
+        return false;
+    }
+}
